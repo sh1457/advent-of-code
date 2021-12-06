d4503a8fb942838fa7314b66403a315109de8848
Sujith Sudarshan <sujith.sudarshan@example.com>
2021-12-07 01:50:51 +0530

    Solved part 2

---
+++ 2021/day2/solution.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+from typing import List, NamedTuple
+from pathlib import Path
+
+from solver import TestCase, log_time, test
+
+
+class Instruction(NamedTuple):
+    move: str
+    units: int
+
+    @staticmethod
+    def from_line(line: str) -> Instruction:
+        move, units = map(str.strip, line.split(' '))
+        units = int(units)
+
+        return Instruction(move, units)
+
+
+@log_time
+def solution(inputs: List[Instruction]) -> int:
+    horizontal, depth = 0,0
+    for instruction in inputs:
+        if instruction.move == "forward":
+            horizontal += instruction.units
+
+        if instruction.move == "up":
+            depth -= instruction.units
+
+        if instruction.move == "down":
+            depth += instruction.units
+
+    return horizontal * depth
+
+
+@log_time
+def solution2(inputs: List[Instruction]) -> int:
+    horizontal, depth, aim = 0,0,0
+    for instruction in inputs:
+        if instruction.move == "forward":
+            horizontal += instruction.units
+            depth += aim * instruction.units
+
+        if instruction.move == "up":
+            aim -= instruction.units
+
+        if instruction.move == "down":
+            aim += instruction.units
+
+    return horizontal * depth
+
+
+def main():
+    base_path = Path(__file__).parent
+    tests = [TestCase.setup(base_path / "sample.txt", 150),
+             TestCase.setup(base_path / "input.txt", 1389)]
+
+    test(solution, tests, Instruction.from_line)
+
+    tests = [TestCase.setup(base_path / "sample.txt", 900),
+             TestCase.setup(base_path / "input.txt", 2138382217)]
+
+    test(solution2, tests, Instruction.from_line)
+
+
+if __name__ == "__main__":
+    main()
