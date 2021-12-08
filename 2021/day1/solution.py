f1d85e2738c88df0006b950dcac242ec366b13c5
Sujith Sudarshan <sujith.sudarshan@example.com>
2021-12-08 18:17:06 +0530

    Added solution to part 2

--- 2021/day1/solution.py
+++ 2021/day1/solution.py
@@ -14,12 +14,30 @@ def solution(inputs: List[int]) -> int:
     return counter
 
 
+@log_time
+def solution2(inputs: List[int]) -> int:
+    counter = 0
+
+    windows = [sum(inputs[i:i+3]) for i in range(0, len(inputs) - 2)]
+
+    for elem, prev in zip(windows[1:], windows[:-1]):
+        if elem > prev:
+            counter += 1
+
+    return counter
+
+
 def main():
     base_path = Path(__file__).parent
     tests = [TestCase.setup(base_path / "sample.txt", 7),
-             TestCase.setup(base_path / "input.txt", 1389)]
+             TestCase.setup(base_path / "input.txt", 1390)]
 
-    test(solution, tests)
+    test(solution, tests, lambda x: int(x.strip()))
+
+    tests = [TestCase.setup(base_path / "sample.txt", 5),
+             TestCase.setup(base_path / "input.txt", 1457)]
+
+    test(solution2, tests, lambda x: int(x.strip()))
 
 
 if __name__ == "__main__":
